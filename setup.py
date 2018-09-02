8f1ccf07daaed922afb12b06061f412aa74282ec
Lennart Regebro <regebro@gmail.com>
2018-09-02 19:26:46 +0200

    Bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from io import open
 from setuptools import setup, find_packages
 
-version = '0.0.7'
+version = '0.0.9'
 
 with open('README.rst', 'rt', encoding='utf8') as readme:
     description = readme.read()
